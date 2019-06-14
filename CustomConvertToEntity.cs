782887696f8fc36fcee64ed15f2bfd59999c4a26
Knightmore <35805408+Knightmore@users.noreply.github.com>
2019-06-14 15:00:12 +0200

    Refactor and proper Injection
    Removed the copied methods of the original ConvertToEntity to only has the necessary methods inside.
    It is now working properly with multiple worlds and injection.

--- CustomConvertToEntity.cs
+++ CustomConvertToEntity.cs
@@ -1,7 +1,6 @@
-using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Unity.Entities
@@ -21,19 +20,28 @@ namespace Unity.Entities
         private static MethodInfo gameObjectToConvertedEntity;
         private static MethodInfo createConversionWorld;
         private static MethodInfo convert;
+        private static MethodInfo injectOriginalComponentsReturnBool;
+        private static MethodInfo injectOriginalComponents;
+        private static MethodInfo addRecurse;
+        public bool hasRun;
 
         void Awake()
         {
-            gameObjectToConvertedEntity = typeof(GameObjectConversionUtility).GetMethod("GameObjectToConvertedEntity", BindingFlags.NonPublic | BindingFlags.Static);
-            createConversionWorld       = typeof(GameObjectConversionUtility).GetMethod("CreateConversionWorld",       BindingFlags.NonPublic | BindingFlags.Static);
-            convert                     = typeof(GameObjectConversionUtility).GetMethod("Convert",                     BindingFlags.NonPublic | BindingFlags.Static);
+            if (hasRun) return;
+            gameObjectToConvertedEntity        = typeof(GameObjectConversionUtility).GetMethod("GameObjectToConvertedEntity", BindingFlags.NonPublic | BindingFlags.Static);
+            createConversionWorld              = typeof(GameObjectConversionUtility).GetMethod("CreateConversionWorld", BindingFlags.NonPublic       | BindingFlags.Static);
+            convert                            = typeof(GameObjectConversionUtility).GetMethod("Convert", BindingFlags.NonPublic                     | BindingFlags.Static);
+            injectOriginalComponentsReturnBool = typeof(ConvertToEntity).GetMethods().FirstOrDefault(x => x.Name == "InjectOriginalComponents" && x.ReturnType == typeof(bool));
+            injectOriginalComponents           = typeof(ConvertToEntity).GetMethod("InjectOriginalComponents", BindingFlags.NonPublic | BindingFlags.Static);
+            addRecurse                         = typeof(ConvertToEntity).GetMethod("AddRecurse", BindingFlags.NonPublic               | BindingFlags.Static);
 
+            var       gameObjectName = gameObject.name;
             if (World.Active != null && CreateToWorld.Count > 0)
             {
                 // Root ConvertToEntity is responsible for converting the whole hierarchy
                 if (transform.parent != null && transform.parent.GetComponentInParent<CustomConvertToEntity>() != null)
                     return;
-
+                
                 foreach (var world in CreateToWorld)
                 {
                     var convertToWorld = World.AllWorlds.FirstOrDefault(x => x.Name == world);
@@ -42,9 +50,16 @@ namespace Unity.Entities
                         if (ConversionMode == Mode.ConvertAndDestroy)
                             ConvertHierarchy(gameObject, convertToWorld);
                         else
-                            ConvertAndInjectOriginal(gameObject, convertToWorld);
+                        {
+                            hasRun = true;
+                            gameObject.GetComponent<CustomConvertToEntity>().enabled = false;
+                            var tempGameObject = Instantiate(gameObject);
+                            tempGameObject.name = gameObjectName + " (" + world + ")";
+                            ConvertAndInjectOriginal(tempGameObject, convertToWorld);
+                        }
                     }
                 }
+                Destroy(gameObject);
             }
             else
             {
@@ -52,61 +67,15 @@ namespace Unity.Entities
             }
         }
 
-        static void InjectOriginalComponents(EntityManager entityManager, Entity entity, Transform transform)
-        {
-            foreach (var com in transform.GetComponents<Component>())
-            {
-                if (com is GameObjectEntity || com is CustomConvertToEntity || com is ComponentDataProxyBase)
-                    continue;
-
-                entityManager.AddComponentObject(entity, com);
-            }
-        }
-
-        public static void AddRecurse(EntityManager manager, Transform transform)
-        {
-            GameObjectEntity.AddToEntityManager(manager, transform.gameObject);
-
-            var convert = transform.GetComponent<CustomConvertToEntity>();
-            if (convert != null && convert.ConversionMode == Mode.ConvertAndInjectGameObject)
-                return;
-
-            foreach (Transform child in transform)
-                AddRecurse(manager, child);
-        }
-
-        public static bool InjectOriginalComponents(World srcGameObjectWorld, EntityManager simulationWorld, Transform transform)
-        {
-            var convert = transform.GetComponent<CustomConvertToEntity>();
-
-            if (convert != null && convert.ConversionMode == Mode.ConvertAndInjectGameObject)
-            {
-                var entity = (Entity)gameObjectToConvertedEntity.Invoke(null, new object[] { srcGameObjectWorld, transform.gameObject});
-                InjectOriginalComponents(simulationWorld, entity, transform);
-                transform.parent = null;
-                return true;
-            }
-
-            for (int i = 0; i < transform.childCount;)
-            {
-                if (!InjectOriginalComponents(srcGameObjectWorld, simulationWorld, transform.GetChild(i)))
-                    i++;
-            }
-
-            return false;
-        }
-
         public static void ConvertHierarchy(GameObject root, World convertToWorld)
         {
-            var gameObjectWorld = (World)createConversionWorld.Invoke(null, new object[] {convertToWorld, default(Hash128), GameObjectConversionUtility.ConversionFlags.AssignName});
+            var gameObjectWorld = (World) createConversionWorld.Invoke(null, new object[] {convertToWorld, default(Hash128), GameObjectConversionUtility.ConversionFlags.AssignName});
 
-            AddRecurse(gameObjectWorld.EntityManager, root.transform);
+            addRecurse.Invoke(null, new object[] { gameObjectWorld.EntityManager, root.transform });
 
             convert.Invoke(null, new object[] {gameObjectWorld, convertToWorld});
 
-            InjectOriginalComponents(gameObjectWorld, convertToWorld.EntityManager, root.transform);
-
-            GameObject.Destroy(root);
+            injectOriginalComponentsReturnBool.Invoke(null, new object[] { gameObjectWorld, convertToWorld.EntityManager, root.transform });
 
             gameObjectWorld.Dispose();
         }
@@ -114,14 +83,14 @@ namespace Unity.Entities
 
         public static void ConvertAndInjectOriginal(GameObject root, World convertToWorld)
         {
-            var gameObjectWorld = (World)createConversionWorld.Invoke(null, new object[] {convertToWorld, default(Hash128), GameObjectConversionUtility.ConversionFlags.AssignName});
+            var gameObjectWorld = (World)createConversionWorld.Invoke(null, new object[] { convertToWorld, default(Hash128), GameObjectConversionUtility.ConversionFlags.AssignName });
 
             GameObjectEntity.AddToEntityManager(gameObjectWorld.EntityManager, root);
 
-            convert.Invoke(null, new object[] {gameObjectWorld, convertToWorld});
+            convert.Invoke(null, new object[] { gameObjectWorld, convertToWorld });
 
             var entity = (Entity)gameObjectToConvertedEntity.Invoke(null, new object[] { gameObjectWorld, root });
-            InjectOriginalComponents(convertToWorld.EntityManager, entity, root.transform);
+            injectOriginalComponents.Invoke(null, new object[] { convertToWorld.EntityManager, entity, root.transform });
 
             gameObjectWorld.Dispose();
         }
